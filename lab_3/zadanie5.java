e3ad5f8c6646fe999c20e7561bbd3d4877418040
SedunovNA <90080517+SedunovNA@users.noreply.github.com>
2021-10-01 15:33:46 +0700

    Create zadanie5.java

---
+++ lab_3/zadanie5.java
@@ -0,0 +1,19 @@
+/*
+ * Составьте программу для нахождения длины катета прямоугольного треугольника (b), если известны длины гипотенузы (c) и катета (a). Результат вывести на консоль..
+ */
+package LAB3;
+
+/**
+ *
+ * @author Uroboros
+ */
+import java.util.Scanner;
+public class Zadanie5 {
+    public static void main (String args[]) {
+        System.out.println("Введите длину гипотенузы и катета:");
+        Scanner sc = new Scanner(System.in);
+        double c = sc.nextDouble();
+        double a = sc.nextDouble();
+        System.out.println("Второй катет = " + Math.sqrt((c*c-a*a)));
+    }  
+}
