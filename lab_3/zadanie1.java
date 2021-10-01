2ce0a9842d25eb778c67abb2699f3e9ac696aacd
SedunovNA <90080517+SedunovNA@users.noreply.github.com>
2021-10-01 15:40:18 +0700

    Create zadanie1.java
    ddd

---
+++ lab_3/zadanie1.java
@@ -0,0 +1,23 @@
+/*
+ * Составьте программу расчета 2^n.
+ */
+
+package LAB1;
+
+/**
+ *
+ * @author Uroboros
+ */
+import java.util.Scanner;
+public class Zadanie2 {
+    public static void main (String args[]) {
+    System.out.println("Введите произвольное число для нахождения 2^n:");
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        int fac = 1;
+        for (int i = 1; i < n+1; i++){
+            fac = fac * 2;
+        }
+        System.out.println("Факториал числа "+ n +" = "+fac);
+    }
+}
