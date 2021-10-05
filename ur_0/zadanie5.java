4304d79bcdccf382c8b39ed1690a83172042b5ce
SedunovNA <90080517+SedunovNA@users.noreply.github.com>
2021-10-05 09:17:36 +0700

    Create zadanie5.java
    ddd

---
+++ ur_0/zadanie5.java
@@ -0,0 +1,26 @@
+/*
+*Объявляем переменные
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+
+Требования:
+1. Объяви переменную name типа String.
+2. Объяви переменную age типа int.
+3. Объяви переменную city типа String.
+4. Должны быть объявлены 3 переменные.
+
+ */
+package uraven_0;
+
+/**
+ *
+ * Седунов Н.А.
+ */
+public class Zadanie_5 {
+    public static void main(String[] args) {
+        String name = "Седунов";
+        int age = 20;
+        String city = "Tomsk";
+        System.out.print(name +"\n"+age+"\n" + city);
+    } 
+}
